d0b21938ad5a4611b54db99869fde20b57890a6d
Alejanro Revilla <mashaec@MacBook-Pro-de-Alejanro.local>
2026-05-16 13:57:13 -0500

    finalizando la generacion de numeros utilizando el un list y range.

---
+++ generar_numeros.py
@@ -0,0 +1,12 @@
+#creamos el array que contiene todos los numeros
+numeros = list(range(1, 1_000_001))
+
+print(f"lista de numeros: '{numeros}'")
+
+#Generamos la opcion del numero que el usuario va a buscar.
+buscar = int(input("Ingresa el número a buscar: "))
+
+# if buscar in numeros:
+#     print(f"{buscar} encontrado en la posición {numeros.index(buscar)}")
+# else:
+#     print(f"{buscar} no está en la lista")
