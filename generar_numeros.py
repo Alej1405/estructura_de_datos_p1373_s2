4128731a906b72be9b208d9ceb60f3540f16bd6a
Alejanro Revilla <mashaec@MacBook-Pro-de-Alejanro.local>
2026-05-16 14:02:21 -0500

    finalizando la busqueda secuencial

--- generar_numeros.py
+++ generar_numeros.py
@@ -1,12 +1,22 @@
+import time
+
 #creamos el array que contiene todos los numeros
 numeros = list(range(1, 1_000_001))
 
-print(f"lista de numeros: '{numeros}'")
+#prueba para verificar que el array funcione
+#print(f"lista de numeros: '{numeros}'")
 
-#Generamos la opcion del numero que el usuario va a buscar.
+#generamos la opcion del numero que el usuario va a buscar.
 buscar = int(input("Ingresa el número a buscar: "))
 
-# if buscar in numeros:
-#     print(f"{buscar} encontrado en la posición {numeros.index(buscar)}")
-# else:
-#     print(f"{buscar} no está en la lista")
+#busqueda secuencial o de uno en uno
+inicio = time.time()
+
+if buscar in numeros:
+    print(f"{buscar} encontrado en la posición {numeros.index(buscar)} tardo ")
+else:
+    print(f"{buscar} no está en la lista")
+
+fin = time.time()
+print(f"Tiempo de búsqueda: {fin - inicio:.6f} segundos")
+#fin de la busqueda en secuencial.
