4a4f6af63f763d452bf4e9727f8aa70b9693af9d
Alejanro Revilla <mashaec@MacBook-Pro-de-Alejanro.local>
2026-05-16 14:09:43 -0500

    finalizanfo busqueda binaria

--- generar_numeros.py
+++ generar_numeros.py
@@ -20,3 +20,28 @@ else:
 fin = time.time()
 print(f"Tiempo de búsqueda: {fin - inicio:.6f} segundos")
 #fin de la busqueda en secuencial.
+
+#busqueda binaria
+inicio_binaria = time.time()
+
+izquierda, derecha = 0, len(numeros) - 1
+posicion = -1
+while izquierda <= derecha:
+    medio = (izquierda + derecha) // 2
+    if numeros[medio] == buscar:
+        posicion = medio
+        break
+    elif numeros[medio] < buscar:
+        izquierda = medio + 1
+    else:
+        derecha = medio - 1
+
+fin_binaria = time.time()
+
+if posicion != -1:
+    print(f"{buscar} encontrado en la posición {posicion} (binaria)")
+else:
+    print(f"{buscar} no está en la lista (binaria)")
+
+print(f"Tiempo de búsqueda binaria: {fin_binaria - inicio_binaria:.6f} segundos")
+#fin de la busqueda binaria.
